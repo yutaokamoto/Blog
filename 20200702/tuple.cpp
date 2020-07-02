73139a792657d7516eb339518bc76d9c4d5b1cb9
yutaokamoto <yutaokamoto1997@gmail.com>
2020-07-02 22:54:29 +0900

    added statements

--- 20200702/tuple.cpp
+++ 20200702/tuple.cpp
@@ -6,12 +6,12 @@ int main(){
     cout << get<0>(t0) << endl;
     cout << get<1>(t0) << endl;
 
-    //なぜかこういうのはできない
+    //なぜかこういうのはできない．
     /*for(int i = 0; i < 2; i++){
         cout << get<i>(t0) << endl;
     }*/
 
-    tuple<int, int> t1(2, 1);
+    tuple<int, int> t1(2, 1); //これでも初期化できる．
     cout << get<0>(t1) << endl;
     cout << get<1>(t1) << endl;
 
