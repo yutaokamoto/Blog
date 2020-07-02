abfceabfa414dcdf1b341a0de1a60b2ac59bf0bf
yutaokamoto <yutaokamoto1997@gmail.com>
2020-07-02 22:53:51 +0900

    new!

---
+++ 20200702/tuple.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    tuple<int, int> t0 = {0,1};
+    cout << get<0>(t0) << endl;
+    cout << get<1>(t0) << endl;
+
+    //なぜかこういうのはできない
+    /*for(int i = 0; i < 2; i++){
+        cout << get<i>(t0) << endl;
+    }*/
+
+    tuple<int, int> t1(2, 1);
+    cout << get<0>(t1) << endl;
+    cout << get<1>(t1) << endl;
+
+    return 0;
+}
