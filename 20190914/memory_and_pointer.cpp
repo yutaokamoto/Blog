1db9b40dd0c06b3bdb0029051af6dd7bf27930a8
yutaokamoto <yutaokamoto1997@gmail.com>
2020-07-19 13:49:01 +0900

    added statements

--- 20190914/memory_and_pointer.cpp
+++ 20190914/memory_and_pointer.cpp
@@ -3,13 +3,15 @@
 #include <mm_malloc.h>
 #include <stdlib.h>
 
+//p.77
 int main()
 {
     short *buf;
         /* ↓malloc()には戻り値がないのでbufと同じ型にキャスト */
-    buf = (short *)malloc(sizeof(short)*2000);
+    buf = (short *)malloc(sizeof(short)*20);
                 /* ↑malloc()は引数で指定したバイト数分のメモリを確保
                     確保できなかったときはNULLを返す */
+    //mallocでメモリを確保した後は配列と小野時ように使える．
     buf[0] = 0;
     buf[2] = 20;
     printf("0番目の要素の値は%d\n", *buf); /* 配列名そのものは、配列の一番最初の要素へのポインタの役割をする */
