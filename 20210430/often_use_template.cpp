2f29fdd83372ee4c8f71e4d3abc87d53ff3c205b
yutaokamoto <yutaokamoto1997@gmail.com>
2021-04-30 22:48:13 +0900

    created chmax template func

--- 20210430/often_use_template.cpp
+++ 20210430/often_use_template.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T>
+bool chmax(T &a, const T &b){
+	if(a<b){
+		a = b;
+		return true;
+	}
+	return false;
+}
+
 int main(){
+	int ans_max = 0;
+	chmax<int>(ans_max, 4);
+	cout << ans_max << endl;
 	return 0;
 }
