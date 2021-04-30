3043b1d8671e449a242bcb4206270158ec0cd633
yutaokamoto <yutaokamoto1997@gmail.com>
2021-04-30 23:19:29 +0900

    created a chmin template func

--- 20210430/often_use_template.cpp
+++ 20210430/often_use_template.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// aよりもbが大きいならば、aをbで更新し、trueを返す
 template <typename T>
 bool chmax(T &a, const T &b){
 	if(a<b){
@@ -10,9 +11,31 @@ bool chmax(T &a, const T &b){
 	return false;
 }
 
+// aよりもbが小さいならば、aをbで更新し、trueを返す
+template <typename T>
+bool chmin(T &a, const T &b){
+	if(a>b){
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+// 適当な関数
+int f(int x){
+	return x*x-10;
+}
+
 int main(){
 	int ans_max = 0;
-	chmax<int>(ans_max, 4);
+	int ans_min = 10000000;
+	// 関数f()のある区間における最大値、最小値を求めるのに便利
+	for(int i=0; i<10; i++){
+		chmax(ans_max, f(i));
+		chmin(ans_min, f(i));
+	}
 	cout << ans_max << endl;
+	cout << ans_min << endl;
+	
 	return 0;
 }
