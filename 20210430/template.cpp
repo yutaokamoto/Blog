a383bdefe5238f66ac5713167d9c869a0e1788d8
yutaokamoto <yutaokamoto1997@gmail.com>
2021-04-30 11:37:20 +0900

    created a template func "square"

--- 20210430/template.cpp
+++ 20210430/template.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template<typename T>
+T square(T x){
+	return x*x;
+}
+
 int main(){
+	int a = 10;
+	double b = 2.0;
+
+	cout << square<int>(a) << endl;
+	cout << square<double>(b) << endl;
 	return 0;
 }
