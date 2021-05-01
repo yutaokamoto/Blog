4ac1aef9a608b4287ec6c73dc53b7e06442d0f02
yutaokamoto <yutaokamoto1997@gmail.com>
2021-05-01 22:39:08 +0900

    added descriptions of iterator

--- 20210430/iterator.cpp
+++ 20210430/iterator.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main(){
+	// 少しポインタの復習
+	int a = 0;
+	int* i_p = &a;
+	(*i_p)++;
+	cout << *i_p << endl;
+
+	// イテレータは、ポインタの一般形
+	vector<int> vec(3, 0);
+	// for文の終了判定条件が「!=」であることに注意!
+	for(auto i=vec.begin(); i!=vec.end(); i++){
+		cout << *i << endl;
+	}
 	return 0;
 }
