3c07f9f3621b1c8371c787f1fb9128b3849372a5
yutaokamoto <yutaokamoto1997@gmail.com>
2021-05-03 16:44:15 +0900

    added statements

--- 20210502/sieve_of_eratosthenes.cpp
+++ 20210502/sieve_of_eratosthenes.cpp
@@ -5,9 +5,13 @@ int main(){
 	// n以下の素数を全て求めるアルゴリズム(エラトステネスのふるい)
 	int n;
 	cin >> n;
+	//// 0~nの数のリスト
 	vector<int> data(n+1, 1);
+	//// 結果を入れておくためのリスト
 	vector<int> result(n+1, 0);
+	//// 走査の時に使う変数
 	int m;
+	//// resultの要素にアクセスするための変数
 	int k = 0;
 
 	data.at(0) = 0;
@@ -23,7 +27,8 @@ int main(){
 		}
 		k++;
 	}
-	// 答えの出力
+	
+	//// 答えの出力
 	cout << n << "以下の素数は、" << endl;
 	for(int prime_number : result){
 		if(prime_number==0){continue;}
